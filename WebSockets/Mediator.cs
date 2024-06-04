5a03fedabcd4acfa09159f6b8b0d9ae413b79219
Sile9t <eyesless1jack@gmail.com>
2024-06-04 14:58:33 +0300

    Changed Mediator.Execute method

--- WebSockets/Mediator.cs
+++ WebSockets/Mediator.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebSockets;
 
 namespace Client
 {
     public interface Mediator
     {
-        public Task Execute(Command command);
+        public Task Execute(Message msg);
     }
 }
